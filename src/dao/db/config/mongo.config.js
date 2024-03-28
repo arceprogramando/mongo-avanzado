b93c332a731cc6c2c050aa1faf9b304df2538b80
Felipe Arce <arceprogramando@gmail.com>
2024-03-28 18:15:17 -0300

    feat(backend): :coffin: remove comments

--- src/dao/db/config/mongo.config.js
+++ src/dao/db/config/mongo.config.js
@@ -14,10 +14,8 @@ const configConnection = {
 const mongoDBConnection = async () => {
   try {
     await mongoose.connect(configConnection.url, configConnection.options);
-    // eslint-disable-next-line no-console
     console.log(`======= URL: ${configConnection.url.substring(0, 20)} =======`);
   } catch (error) {
-    // eslint-disable-next-line no-console
     console.log(`🚀 ~ file: mongo.config.js:28 ~ mongoDBConnection ~ err:, ${error}`);
   }
 };
